001974a64889fc0170d9366313462149c3bb19ea
Mohamed <mohamed.el-khadiri@epitech.eu>
2026-01-12 10:56:27 +0100

    ajout du logo dans le home

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import "../App.css";
+import logo from "../img/logo.png";
 
 export default function Home() {
     return (
         <div className="page home-page">
             <section className="hero">
+                <img src={logo} alt="Popcorn Logo" className="home-logo" />
                 <h1>POPCORN</h1>
                 <p>
                     Votre assistant cinéma intelligent propulsé par l'IA.
